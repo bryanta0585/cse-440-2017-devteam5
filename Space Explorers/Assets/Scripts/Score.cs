60f3e65a1246fb71d9d6aadb7c5d9e76a8c1b8af
machucalos <machucaproductions@gmail.com>
2017-10-20 18:44:19 -0700

    Organize basic hierarchy

---
+++ Space Explorers/Assets/Scripts/Score.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour {
+    uint  score;
+    uint maxscore;
+    void ChangeScore(uint newScore)
+    {
+        maxscore = newScore;
+    }
+    void IncreaseScoreBy(uint iScore)
+    {
+        if(score >= maxscore)
+        {
+            score = maxscore;
+        }
+        else
+        {
+            score += iScore;
+        }
+       
+    }
+    void DecreaseScoreBy(uint iScore)
+    {
+        if (score-iScore < 0)
+        {
+            score = 0;
+        }
+        else
+        {
+            score -= iScore;
+        }
+       
+    }
+
+}
