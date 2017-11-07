88da553285a4fe752f04abd7cbd6304e4176ddec
machucalos <machucaproductions@gmail.com>
2017-11-07 11:42:01 -0800

    Menu Scene
    menu

---
+++ Space Explorers/Assets/Scripts/MenuUi.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuUi : MonoBehaviour {
+
+    // Use this for initialization
+    [SerializeField]
+    private GameObject exitMenu, mainMenu,settingsMenu;
+    private void Awake()
+    {
+        
+        exitMenu.SetActive(false);
+        settingsMenu.SetActive(false);
+    }
+
+    public void LoadFirstLevel()
+    {
+        Debug.Log("Loading First level");
+        /*
+        SceneManager.LoadScene(1);
+        SceneManager.LoadScene("SceneName");
+        */
+    }
+    public void SwitchLayout()
+    {
+        current.SetActive(false);
+        newlayout.SetActive(true);
+       
+    }
+    public void Quit()
+    {
+        Debug.Log("Exiting");
+        Application.Quit();
+    }
+}
